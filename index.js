3804647bd32ad396d315614e5c5abf27c7f29c0d
nhcongtru <trub2014627@student.ctu.edu.vn>
2023-11-05 08:49:39 +0700

    ThemDem2Tay,BatTat

--- index.js
+++ index.js
@@ -2,35 +2,11 @@ const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 
-function onResults(results) {
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
-  if (results.multiHandLandmarks) {
-    for (const landmarks of results.multiHandLandmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-                     {color: '#00FF00', lineWidth: 5});
-      drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
-    }
-  }
-  canvasCtx.restore();
-}
-
-const hands = new Hands({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-}});
-hands.setOptions({
-  maxNumHands: 2,
-  modelComplexity: 1,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
-});
-hands.onResults(onResults);
-
+let leftHandCount = 0;
+let rightHandCount = 0;
 
 function countFingers(landmarks) {
-    const thumbTip = landmarks[4];
+  const thumbTip = landmarks[4];
     const thumbIP = landmarks[3];
     const thumbCMC = landmarks[1];
     const wrist = landmarks[0];
@@ -72,46 +48,78 @@ function countFingers(landmarks) {
     }
   
     return fingerCount;
-  }
-  
-  function onResults(results) {
-    canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
-    if (results.multiHandLandmarks) {
-      for (const landmarks of results.multiHandLandmarks) {
-        drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-          { color: '#00FF00', lineWidth: 5 });
-        drawLandmarks(canvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
-  
-        const fingerCount = countFingers(landmarks);
-        canvasCtx.font = '28px Arial';
-        canvasCtx.fillStyle = '#FFFFFF';
-        canvasCtx.fillText(`Số Ngón Tay: ${fingerCount}`, 10, 50);
+}
+
+function onHandResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+    results.image, 0, 0, canvasElement.width, canvasElement.height);
+  if (results.multiHandLandmarks) {
+    for (let handIndex = 0; handIndex < results.multiHandLandmarks.length; handIndex++) {
+      const landmarks = results.multiHandLandmarks[handIndex];
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+        { color: '#00FF00', lineWidth: 5 });
+      drawLandmarks(canvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
+
+      const fingerCount = countFingers(landmarks);
+      canvasCtx.font = '28px Arial';
+      canvasCtx.fillStyle = '#FFFFFF';
+
+      if (landmarks[0].x > landmarks[9].x) {
+        leftHandCount = fingerCount;
+        canvasCtx.fillText(`Số Ngón Tay Trái: ${leftHandCount}`, 700, 50);
+      } else {
+        rightHandCount = fingerCount;
+        canvasCtx.fillText(`Số Ngón Tay Phải: ${rightHandCount}`,10, 50);
       }
     }
-    canvasCtx.restore();
   }
-  
+  canvasCtx.restore();
+}
 
-  hands.setOptions({
-    maxNumHands: 2,
-    modelComplexity: 1,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
-  });
-  hands.onResults(onResults);
-  
-  hands.initialize().then(() => {
-    hands.start();
-  });
-  
-  const camera = new Camera(videoElement, {
-    onFrame: async () => {
-      await hands.send({ image: videoElement });
-    },
-    width: 640,
-    height: 360
-  });
-  camera.start();
+
+const hands = new Hands({ locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}});
+hands.setOptions({
+  maxNumHands: 2,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+hands.onResults(onHandResults);
+
+hands.initialize().then(() => {
+  hands.start();
+});
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await hands.send({ image: videoElement });
+  },
+  width: 640,
+  height: 360
+});
+camera.start();
+
+videoElement.style.display = "none";
+canvasElement.style.display = "none";
+
+// Bật/tắt camera
+function toggleCamera() {
+  if (videoElement.style.display === "none") {
+    videoElement.style.display = "block";
+  } else {
+    videoElement.style.display = "none";
+  }
+}
+
+// Bật/tắt canvas
+function toggleCanvas() {
+  if (canvasElement.style.display === "none") {
+    canvasElement.style.display = "block";
+  } else {
+    canvasElement.style.display = "none";
+  }
+}
