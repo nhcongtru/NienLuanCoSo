00943d4d9f972c8a9f58bd7f8ee5c6dbf056aa44
nhcongtru <trub2014627@student.ctu.edu.vn>
2023-11-24 22:08:38 +0700

    Final

--- index.js
+++ index.js
@@ -1,58 +1,159 @@
-const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
-const canvasCtx = canvasElement.getContext('2d');
 
 let leftHandCount = 0;
 let rightHandCount = 0;
 
 function countFingers(landmarks) {
+  const wrist = landmarks[0];
   const thumbTip = landmarks[4];
-    const thumbIP = landmarks[3];
-    const thumbCMC = landmarks[1];
-    const wrist = landmarks[0];
-    const thumb = landmarks[4];
-    const pinky = landmarks[20];
+  const thumbIP = landmarks[3];
 
-    let fingerCount = 0;
-  
-    // Check if thumb is extended
+  let fingerCount = 0;
 
-    if (wrist.x < thumb.x) {
-        if (thumbTip.x > thumbIP.x) {
-              fingerCount++;
-            }
-    } else if (wrist.x > thumb.x) {
-        if (thumbTip.x < thumbIP.x) {
-            fingerCount++;
-        }
+  if (isThumbExtended(landmarks)) {
+    fingerCount++;
+  }
+
+  const fingerTips = [8, 12, 16, 20];
+
+  for (const tipIdx of fingerTips) {
+    const fingerTip = landmarks[tipIdx];
+    const prevFingerJoint = landmarks[tipIdx - 3];
+    const nextFingerJoint = landmarks[tipIdx - 1];
+
+    if (isFingerTipExtended(fingerTip, prevFingerJoint, nextFingerJoint)) {
+      fingerCount++;
     }
+  }
+
+  return fingerCount;
+}
+
+function isThumbExtended(landmarks) {
+  const thumbTip = landmarks[4];
+  const thumbIP = landmarks[3];
+
+  if (landmarks[0].x > landmarks[9].x) {
+    return thumbTip.x < thumbIP.x;
+  } else {
+    return thumbTip.x > thumbIP.x;
+  }
+}
+
+function isFingerTipExtended(point1, point2, referencePoint) {
+  const distanceToReference = calculateDistance(point1, referencePoint);
+
+  return distanceToReference > 0.03 && point1.y < point2.y;
+}
+
+function calculateDistance(point1, point2) {
+  return Math.sqrt(
+    Math.pow(point1.x - point2.x, 2) +
+    Math.pow(point1.y - point2.y, 2) +
+    Math.pow(point1.z - point2.z, 2)
+  );
+}
 
 
+//IMAGE
 
-    const fingerTips = [8, 12, 16, 20];
-  
-    for (const tipIdx of fingerTips) {
-      const fingerTip = landmarks[tipIdx];
-      const prevFingerJoint = landmarks[tipIdx - 2];
-      const nextFingerJoint = landmarks[tipIdx - 1];
-  
-      const distance = Math.sqrt(
-        Math.pow(fingerTip.x - prevFingerJoint.x, 2) +
-        Math.pow(fingerTip.y - prevFingerJoint.y, 2) +
-        Math.pow(fingerTip.z - prevFingerJoint.z, 2)
-      );
-  
-      if (distance > 0.03 && fingerTip.y < nextFingerJoint.y) {
-        fingerCount++;
+const canvasElementIMG = document.getElementById('outputCanvas');
+const canvasCtxIMG = canvasElementIMG.getContext('2d');
+
+function clearCanvas() {
+  canvasCtxIMG.clearRect(0, 0, canvasElementIMG.width, canvasElementIMG.height);
+}
+
+function onHandResultsIMG(results) {
+  canvasCtxIMG.save();
+  clearCanvas();
+
+  canvasCtxIMG.drawImage(
+    results.image, 0, 0, canvasElementIMG.width, canvasElementIMG.height);
+
+  if (results.multiHandLandmarks) {
+    for (let handIndex = 0; handIndex < results.multiHandLandmarks.length; handIndex++) {
+      const landmarks = results.multiHandLandmarks[handIndex];
+      drawConnectors(canvasCtxIMG, landmarks, HAND_CONNECTIONS,
+        { color: '#00FF00', lineWidth: 3 });
+      drawLandmarks(canvasCtxIMG, landmarks, { color: '#FF0000', lineWidth: 1 });
+
+      const fingerCount = countFingers(landmarks);
+      canvasCtxIMG.font = '28px Arial';
+      canvasCtxIMG.fillStyle = '#FF0000';
+
+      if (landmarks[0].x > landmarks[9].x) {
+        leftHandCount = fingerCount;
+        canvasCtxIMG.fillText(`Số Ngón Tay Trái: ${leftHandCount}`, 640, 50);
+      } else {
+        rightHandCount = fingerCount;
+        canvasCtxIMG.fillText(`Số Ngón Tay Phải: ${rightHandCount}`, 5, 50);
       }
     }
-  
-    return fingerCount;
+  }
+
+  canvasCtxIMG.restore();
 }
+
+const handsIMG = new Hands({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+  }
+});
+handsIMG.setOptions({
+  maxNumHands: 2,
+  modelComplexity: 1,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+handsIMG.onResults(onHandResultsIMG);
+
+const inputImage = document.getElementById('inputImage');
+
+inputImage.addEventListener('change', function () {
+  chooseFile(this);
+});
+
+function chooseFile(fileInput) {
+  if (fileInput.files && fileInput.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      const image = new Image();
+      image.src = e.target.result;
+      onImageResults(image);
+    }
+    reader.readAsDataURL(fileInput.files[0]);
+  }
+}
+
+function onImageResults(image) {
+  canvasCtxIMG.save();
+  clearCanvas();
+
+  canvasCtxIMG.drawImage(image, 0, 0, canvasElementIMG.width, canvasElementIMG.height);
+  handsIMG.send({ image });
+  canvasCtxIMG.restore();
+}
+
+function deleteImage() {
+  if (canvasElementIMG.style.display === "none") {
+    canvasElementIMG.style.display = "block";
+  } else {
+    canvasElementIMG.style.display = "none";
+  }
+}
+canvasElementIMG.style.display = "none";
+
+
+//WEBCAM
+
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
 
 function onHandResults(results) {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
   canvasCtx.drawImage(
     results.image, 0, 0, canvasElement.width, canvasElement.height);
   if (results.multiHandLandmarks) {
@@ -64,24 +165,25 @@ function onHandResults(results) {
 
       const fingerCount = countFingers(landmarks);
       canvasCtx.font = '28px Arial';
-      canvasCtx.fillStyle = '#FFFFFF';
+      canvasCtx.fillStyle = '#FF0000';
 
       if (landmarks[0].x > landmarks[9].x) {
         leftHandCount = fingerCount;
         canvasCtx.fillText(`Số Ngón Tay Trái: ${leftHandCount}`, 700, 50);
       } else {
         rightHandCount = fingerCount;
-        canvasCtx.fillText(`Số Ngón Tay Phải: ${rightHandCount}`,10, 50);
+        canvasCtx.fillText(`Số Ngón Tay Phải: ${rightHandCount}`, 10, 50);
       }
     }
   }
   canvasCtx.restore();
 }
 
-
-const hands = new Hands({ locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-}});
+const hands = new Hands({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+  }
+});
 hands.setOptions({
   maxNumHands: 2,
   modelComplexity: 1,
@@ -106,20 +208,12 @@ camera.start();
 videoElement.style.display = "none";
 canvasElement.style.display = "none";
 
-// Bật/tắt camera
-function toggleCamera() {
-  if (videoElement.style.display === "none") {
+function toggle() {
+  if (videoElement.style.display === "none" && canvasElement.style.display === "none") {
     videoElement.style.display = "block";
-  } else {
-    videoElement.style.display = "none";
-  }
-}
-
-// Bật/tắt canvas
-function toggleCanvas() {
-  if (canvasElement.style.display === "none") {
     canvasElement.style.display = "block";
   } else {
+    videoElement.style.display = "none";
     canvasElement.style.display = "none";
   }
 }
